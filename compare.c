d46e708933f5c069041df34068d4586d52816fd6
John Dunthorne <johndunthorne1@gmail.com>
2023-09-30 15:16:54 -0500

    1

---
+++ compare.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int x;
+    int y;
+
+    printf("Type first number: ");
+    scanf("%d", &x);
+    printf("Type second number: ");
+    scanf("%d", &y);
+}
+
