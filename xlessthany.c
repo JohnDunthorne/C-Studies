5ee831fd18ff66297d1f3691db705d9635a7a68b
John Dunthorne <johndunthorne1@gmail.com>
2023-09-30 12:06:56 -0500

    1

--- xlessthany.c
+++ xlessthany.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
 int main() {
-int x = 20;
-int y = 18;
+
+
+
+int x;
+int y;
+
+// Ask the user to type a number AND a character
+printf("Type a number for X press enter \nAND a number for Y and press enter: \n");
+
+// Get and save the number AND character the user types
+scanf("%d %d", &x, &y);
+
+// Print the number
+printf("X is: %d\n", x);
+
+// Print the character
+printf("Y is: %d\n", y);
+
 if (x > y) {
-  printf("x is greater than y");
+  printf("X is greater than Y");
+} else if (x < y) {
+  printf("X is not greater than Y");
+} else {
+  printf("X is equal to Y");  
 }
 }
