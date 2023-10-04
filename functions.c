f22f4c08d154b75135d29397c0c536828221aac3
John Dunthorne <johndunthorne1@gmail.com>
2023-10-04 18:31:44 -0500

    1

--- functions.c
+++ functions.c
@@ -27,7 +27,9 @@ void print_grid(int size)
     for (int i = 0; i < size; i++)
     {
         for (int j = 0; j < size; j++)
-        printf("#");
+        {
+            printf("#");
+        }
+        printf("\n");
     }
-    printf("\n");
 }
