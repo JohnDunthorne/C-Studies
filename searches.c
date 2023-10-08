5732270eb4556164dc2cf9d93f63bdf750a86f1d
John Dunthorne <johndunthorne1@gmail.com>
2023-10-08 13:46:16 -0500

    1

---
+++ searches.c
@@ -0,0 +1,20 @@
+# include <stdio.h>
+// find a number using LINEAR search.
+int main (void)
+{
+    int numbers[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+    int selection;
+    printf("please select a number between 1 and 20: ");
+    scanf("%i", &selection);
+
+    for (int i = 0; i < selection; i++)
+    {
+        if (numbers[i] == selection)
+        {
+            printf("Found on %d trys.\n", i + 1);
+            return 0;
+        }
+    }
+    printf("Not found\n");
+    return 1;    
+}
